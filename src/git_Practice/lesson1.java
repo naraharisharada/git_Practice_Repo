3c09056acf2e87e458665c45b75ab234ef5d21b3
naraharisharada <naraharisharada@gmail.com>
2020-05-02 20:18:11 -0700

    second commit on test case lesson1

--- src/git_Practice/lesson1.java
+++ src/git_Practice/lesson1.java
@@ -4,6 +4,7 @@ public class lesson1 {
 
 	public static void main(String[] args) {
 		System.out.println("Practicing git initalisation");
+		System.out.println("add,save and push code to cloud repo");
 	}
 
 }
