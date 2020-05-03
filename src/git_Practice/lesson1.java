52baafb5c4d378d636fca0898207a18fa4664c99
naraharisharada <naraharisharada@gmail.com>
2020-05-02 21:51:21 -0700

    third commit in arha

--- src/git_Practice/lesson1.java
+++ src/git_Practice/lesson1.java
@@ -5,6 +5,7 @@ public class lesson1 {
 	public static void main(String[] args) {
 		System.out.println("Practicing git initalisation");
 		System.out.println("add,save and push code to cloud repo");
+		System.out.println("after creating arha,commiting changes and pushing code - commit 3");
 	}
 
 }
